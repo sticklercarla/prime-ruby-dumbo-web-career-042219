fa3a24984bb72fc698a71fe06ff5c9ffb7261a78
Carla Stickler <carlastickler@gmail.com>
2019-04-13 00:33:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,11 @@
 # Add  code here!
-require 'pry'
+
 def prime? (n)
     if n <= 1
         false
     elsif n == 2
         true
     else 
-      # binding.pry
         (2..n/2).none? { |i| n % i == 0}
     end
 end
